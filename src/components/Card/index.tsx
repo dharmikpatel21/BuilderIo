f91975e8e6de917d3fa01283a9d5ecdd8c1e805e
dharmikpatel21 <dharmikjpatel21@gmail.com>
2024-03-11 15:51:11 +0530

    changes

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,54 +1,18 @@
 import Image from "next/image";
 import React from "react";
 import Title from "../Title";
-// const data = [
-//   {
-//     id: 1,
-//     name: "Smartphone X",
-//     price: 599.99,
-//     description:
-//       "The latest flagship smartphone from TechCorp with advanced features.",
-//     image: "/images/1.jpg",
-//   },
-//   {
-//     id: 2,
-//     name: "Smartphone Y",
-//     price: 699.99,
-//     description: "The latest flagship smartphone.",
-//     image: "/images/2.jpg",
-//   },
-//   {
-//     id: 3,
-//     name: "Smartphone Z",
-//     price: 799.99,
-//     description: "The latest flagship smartphone from TechCorp. ",
-//     image: "/images/3.jpg",
-//   },
-// ];
 
 type Props = {
   title: string;
   image: string;
   description: string;
   price: string;
+  name: string;
 };
 
 const Card = ({ name, title, image, description, price }: Props) => {
   return (
     <>
-      {/* {data.map((item) => {
-        return (
-          <div key={item.id}>
-            <p>{children}</p>
-            <div className="relative aspect-square w-[200px]">
-              <Image src={item.image} alt="image" fill />
-            </div>
-            <p>{item.name}</p>
-            <p>{item.description}</p>
-            <p>{item.price}</p>
-          </div>
-        );
-      })} */}
       <div>
         <Title title={title} />
         <div className="relative aspect-square w-[200px]">
